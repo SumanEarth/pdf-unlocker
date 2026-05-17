2f4260d5b625219c8c46c95cf019f4915390a3d3
SumanEarth <suman.env.bd@gmail.com>
2026-05-17 10:17:48 +0000

    Add service worker for caching assets and offline support

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'pdf-unlocker-v1';
+const ASSETS = ['/', '/index.html', '/pdf-lib.min.js', '/icon-192.png', '/icon-512.png', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
